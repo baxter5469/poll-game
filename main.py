9ed80fcd5a8f6ec4bbcbb78ea035b8765bacccdf
Andrew Ault <andrew@baxter.tamz.us>
2018-04-15 19:48:14 -0400

    v2.0 Complete

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-''' Poll Game v1.5 By Andrew Ault
+''' Poll Game v2.0 By Andrew Ault
 4/12/18
 '''
 import json
-names = {}
+import ast
+try:
+  f = open('file.txt','r')
+  names = ast.literal_eval((f.read()))
+  f.close()
+except:
+  names = {}
 while True:
     username = input(str("Please type your name: ")).lower()
+    if username == "quit":
+        break
     if username.lower() in names:
         print("Name already in the list, Try again.")
         continue
-    if username == "quit":
-        break
     if len(username) < 1:
         print("Type quit if you would like to move on!")
     if len(username) > 1:
@@ -21,6 +27,10 @@ while True:
                 file.write(json.dumps(names))
         except:
             print("Only the number no letters!")
+    names[username.lower()] = userinput
+    f = open('file.txt','w')
+    f.write(str(names))
+    f.close()
 if len(names) > 0:
     print("The average month from your inputs is: " + str(sum(names.values()) / len(names)))
 else:
